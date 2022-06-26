886703a1c84400540800253b0e0cd8dc84ba2e60
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-06-26 23:46:05 +0600

    Добавили программу с решением курсовой задачи

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿// Курсовая работа. Написать программу, которая из имеющегося массива строк формирует новый массив 
+// из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+// Объявляем массив
+string [ ] names = new string [5];
+int size = 5;
+string [ ] massiv = new string [size];
+
+// Вводим данные массива с клавиатуры
+Console.WriteLine("Введите данные массива: ");
+for (int i = 0; i < names.Length; i++)
+{
+    Console.Write("names[{0}] = ", i);
+    names[i]=Console.ReadLine();
+}
+for (int i = 0; i < names.Length; i++)
+{
+    if(names[i].Length <=3)
+        {
+            massiv[i] = names[i];
+            Console.Write(massiv[i] + ",");
+        }   
+        else continue;
+}   
